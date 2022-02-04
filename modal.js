6d0f898c685dfbc00c8071e78d90d5bb337bdc48
James <jamesglendenning1@gmail.com>
2022-02-04 17:19:13 +0000

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,34 +1,34 @@
 const modal =  {
 
-    /**
-     * Removes the modal from the DOM.
-     */
+	/**
+	* Removes the modal from the DOM.
+	*/
 	remove: () => {
 		document.getElementById( 'modal' ).remove();
 	},
 
-    /**
-     * Generates the modal & adds close actions.
-     * @param {arrayOfModalItems}  
-     */
+	/**
+	* Generates the modal & adds close actions.
+	* @param {arrayOfModalItems}
+	*/
 	generate: ( { title = '', description = '', removeText = 'Cancel', addText = '', mainContent = '', callback } ) => {
-		
-        // If the content is set then wrap it in a content div.
+
+		// If the content is set then wrap it in a content div.
 		const postContent = mainContent ? `<div id="content" class="modal__content">${mainContent}</div>` : '';
-		
-        // The main modal content.
+
+		// The main modal content.
 		const modal =  `
-        <div id="modal" class="modal">
+		<div id="modal" class="modal">
 			<div class="modal__container">
 				<h3>${title}</h3>
 				${description}
-				
+
 				${postContent}
 
 				<div class="modal-buttons is-flex">
 					<button id="preview-modal__close" class="btn btn--primary">${removeText}</button>
 					<button id="preview-modal__do_it" class="btn btn--primary">${addText}</button>
-				</div>		
+				</div>
 			</div>
 		</div>`;
 
@@ -46,13 +46,13 @@ const modal =  {
 		});
 
 		/**
-		 * When the user clicks escape remove the modal.
-		 * TODO: Fix this, keypress is detected but not for the escape key,
-		 * think it's blocked by another script.
-		 */
+		* When the user clicks escape remove the modal.
+		* TODO: Fix this, keypress is detected but not for the escape key,
+		* think it's blocked by another script.
+		*/
 		document.body.addEventListener( 'keypress', function(e) {
 			const event = e || window.event;
-			
+
 			if ( 'key' in event ? ( event.key === 'Escape' || event.key === 'Esc') : ( event.keyCode === 27 ) ) {
 				modal.remove();
 			}
