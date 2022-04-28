1935ec2b93a5e9e83dfee071ad5125fc54e59cc2
James <jamesglendenning1@gmail.com>
2022-04-28 17:08:28 +0100

    Added remove icon, added conditionals for wrappers, fixed some errors.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ class SuperSimpleModal {
 				}
 			}
 		];
+
+        this.removeIcon = '<svg class="ssm-modal-remove" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#fff" d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg>';
 	}
 
 	/**
@@ -124,16 +126,16 @@ class SuperSimpleModal {
     		aria-describedby="ssm-modal__description"
 			aria-hidden="false"
 		>
+            ${this.removeIcon}
+
 			<div class="ssm-modal__container">
-				<h3 id="ssm-modal__title">${this.strip(title)}</h3>
+				${title ? `<h3 id="ssm-modal__title">${this.strip(title)}</h3>` : ''}
 
 				${outputDescription}
 
 				${postContent}
 
-				<div class="ssm-modal-buttons is-flex">
-					${filterXSS(buttons)}
-				</div>
+				${buttons && `<div class="ssm-modal-buttons is-flex">${filterXSS(buttons)}</div>`}
 			</div>
 		</div>`;
 
@@ -144,8 +146,10 @@ class SuperSimpleModal {
 		this.initiateFocusTrap( 'ssm-modal' );
 
 		// Add the cancel modal open action.
-		document.getElementById( 'ssm-modal__na' ).addEventListener( 'click', () => this.remove() );
-
+        if ( document.getElementById( 'ssm-modal__na' ) ) {
+            document.getElementById( 'ssm-modal__na' ).addEventListener( 'click', () => this.remove() );
+        }
+		
 		// Remove the modal if they click the grey area.
 		document.getElementById( 'ssm-modal' ).addEventListener( 'click', (e) => {
 			if ( 'ssm-modal' === e.target.getAttribute('id') ) {
@@ -222,8 +226,10 @@ class SuperSimpleModal {
 		const firstFocusableEl = selectableItems[0];
 		const lastFocusableEl = selectableItems[ selectableItems.length - 1 ];
 
-		// Focus the first focusable item.
-		firstFocusableEl.focus();
+		if ( firstFocusableEl ) {
+            // Focus the first focusable item.
+            firstFocusableEl.focus();
+        }
 
 		// Add a keydown event listener to the modal.
 		parentContainer.addEventListener('keydown', (e) => {
